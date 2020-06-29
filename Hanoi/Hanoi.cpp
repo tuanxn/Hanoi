1110289650f5a20fd02cceec2e77dcc9ac173275
tuanxn <tuan.uci@gmail.com>
2020-06-29 07:11:56 -0700

    Add project files.

---
+++ Hanoi/Hanoi.cpp
@@ -0,0 +1,8 @@
+// Student ID: 20274909
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ Hanoi/Hanoi.h
@@ -0,0 +1,25 @@
+// Student ID: 20274909
+
+#ifndef Hanoi_h
+#define Hanoi_h
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Hanoi {
+private:
+   int _num_poles;
+   int _num_discs;
+   // TODO: Declare the _cache member using an appropriate
+   // level of nesting within std::vectors to put each string
+   // of moves. You should be able to access the cache like so:
+   // _cache[num_discs][src][dst] = "move1\nmove2\n..."
+   std::string lookup_moves(int num_discs, int src, int dst);
+   std::string get_moves(int num_discs, int src, int dst, int tmp);
+public:
+   // Use freebie default constructor
+   std::string solve(int num_discs, int src, int dst, int tmp);
+   friend class Tests; // Don't remove this line
+};
+
+#endif Hanoi_h

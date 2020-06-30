a0361adf3d3f402527734739065106807b2a4b3a
tuanxn <tuan.uci@gmail.com>
2020-06-30 06:28:25 -0700

    Updated for everything but _cache

--- Hanoi/Hanoi.cpp
+++ Hanoi/Hanoi.cpp
@@ -1,8 +1,43 @@
 // Student ID: 20274909
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "Hanoi.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   Hanoi test = Hanoi();
+   cout << test.solve(3, 1, 2, 3);
+}
+
+std::string Hanoi::lookup_moves(int num_discs, int src, int dst) {
+   return "";
+}
+
+std::string Hanoi::get_moves(int num_discs, int src, int dst, int tmp) {
+   string output = "";
+   
+   if (num_discs == 1) {
+      output += to_string(src) + "->" + to_string(dst) + "\n";
+      return output;
+   }
+   else if (num_discs == 0) {
+      return output;
+   }
+   else {
+      output += get_moves(num_discs - 1, src, tmp, dst);
+      output += to_string(src) + "->" + to_string(dst) + "\n";
+      output += get_moves(num_discs - 1, tmp, dst, src);
+   }
+   return output;
+}
+
+std::string Hanoi::solve(int num_discs, int src, int dst, int tmp) {
+   string output = "# Below, 'A->B' means 'move the top disc on pole A to pole B'\n";
+   output += get_moves(num_discs, src, dst, tmp);
+   return output;
 }
